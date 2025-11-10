2c9a668a6cceee608b7d6f8e5586be32b8c27c7d
Mujtaba Javaid <mujtabajavaid@synaphis.com>
2025-11-11 03:20:09 +0500

    Fix Chrome installation on Render

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-set -e
-echo "Installing Puppeteer bundled Chromium..."
-# This installs the Chromium binary used by puppeteer on CI/deploy hosts.
-npx puppeteer install
-echo "Puppeteer install complete"
+npm install
+npx puppeteer browsers install chrome
